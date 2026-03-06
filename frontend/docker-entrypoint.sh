f3d4e014fbd4f18a324c774c6831bce1c528850a
Travelling Coder <sg.gajula@gmail.com>
2026-03-05 19:28:07 -0500

    nginx

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 # Railway injects PORT; default to 80 for local Docker
 PORT="${PORT:-80}"
-sed "s/LISTEN_PORT/${PORT}/" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-exec nginx -g "daemon off;"
+sed "s/LISTEN_PORT/${PORT}/" /etc/nginx/conf.d/default.conf.template > /tmp/default.conf
+exec nginx -c /tmp/nginx.conf -g "daemon off;"
