3d388d0266edc9e7b9759397507cb181000ad5c2
Travelling Coder <sg.gajula@gmail.com>
2026-03-05 20:41:15 -0500

    Changed from nginx to serve on PORT

--- frontend/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-# Railway injects PORT; default to 80 for local Docker
-PORT="${PORT:-80}"
-sed "s/LISTEN_PORT/${PORT}/" /etc/nginx/conf.d/default.conf.template > /tmp/default.conf
-exec nginx -c /tmp/nginx.conf -g "daemon off;"
